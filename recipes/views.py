2241b9abd996abcc110617b0b9bf586790c971cf
João Ramyllo Guedes <joaoramyllo.dev@gmail.com>
2024-03-21 21:48:16 -0300

    Add base_templates

---
+++ recipes/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'recipes/home.html', context={
+        'name': 'Joao Ramyllo'
+    })
