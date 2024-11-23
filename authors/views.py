131779da9673251a1f7d8478078b7061b3edaccc
João Ramyllo Guedes <joaoramyllo.dev@gmail.com>
2024-11-23 15:25:59 -0300

    Criando um Formulário de Login

--- authors/views.py
+++ authors/views.py
@@ -1,20 +1,46 @@
-import re
+from django.contrib import messages
 from django.http import Http404
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect, render
+from django.urls import reverse
 
-from .forms import RegisterForm
+from .forms import LoginForm, RegisterForm
 
 
-def register_view(request): #basicamnete um mockup
+def register_view(request):
     register_form_data = request.session.get('register_form_data', None)
     form = RegisterForm(register_form_data)
-    return render(request, 'authors/pages/register_view.html', {'form': form,})
+    return render(request, 'authors/pages/register_view.html', {
+        'form': form,
+        'form_action': reverse('authors:register_create'),
+    })
 
 
-def register_create(request): #vai ler os dados do post, validar, e criar o usuario e se nao estiver correto, retorna um erro
+def register_create(request):
     if not request.POST:
         raise Http404()
+
     POST = request.POST
     request.session['register_form_data'] = POST
     form = RegisterForm(POST)
+
+    if form.is_valid():
+        user = form.save(commit=False)
+        user.set_password(user.password)
+        user.save()
+        messages.success(request, 'Your user is created, please log in.')
+
+        del(request.session['register_form_data'])
+
     return redirect('authors:register')
+
+
+def login_view(request):
+    form = LoginForm()
+    return render(request, 'authors/pages/login.html', {
+        'form': form,
+        'form_action': reverse('authors:login_create')
+    })
+
+
+def login_create(request):
+    return render(request, 'authors/pages/login.html')
