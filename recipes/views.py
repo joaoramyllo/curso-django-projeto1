737158a49a8f4cab66e6a710ca77f0f02fcf57da
João Ramyllo Guedes <joaoramyllo.dev@gmail.com>
2025-10-27 16:45:30 -0300

    Uma nova URL para uma nova View

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 
-# from django.http import HttpResponse
-
 # Create your views here.
 
 
 def home(request):
-    return render(request, "recipes/pages/home.html", context={"name": "André"})
+    return render(request, "recipes/pages/home.html")
+
+
+def recipe(request, id):
+    return render(request, "recipes/pages/home.html")
