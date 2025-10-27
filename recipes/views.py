b6d327675af5e753f37efc9af9a9ace39c45274b
João Ramyllo Guedes <joaoramyllo.dev@gmail.com>
2025-10-26 22:13:34 -0300

    Iniciando o header e separando o partial head

--- recipes/views.py
+++ recipes/views.py
@@ -6,4 +6,4 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, "recipes/home.html", context={"name": "André"})
+    return render(request, "recipes/pages/home.html", context={"name": "André"})
